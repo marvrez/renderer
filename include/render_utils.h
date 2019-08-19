f036b1fb0c74b55496e30cdc9e9b7992e15cebff
Marvin Reza <marvinreza@outlook.com>
2019-08-19 13:55:33 +0200

    add render utils

--- include/render_utils.h
+++ include/render_utils.h
@@ -6,7 +6,23 @@
 #include "math_utils.h"
 #include "physics_utils.h"
 
-void render_wall(wall_line l);
+typedef struct {
+    player p;
+    SDL_Point virtual_joystick_pos;
+    int running;
+
+    // SDL states
+    SDL_Surface* surface;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
+} program_state;
+
+void render_wall(wall_line wl);
+void render_debug_wall(wall_line wl);
+
+void draw_views(SDL_Renderer* renderer, SDL_Point* offset);
+void draw_debug_text(SDL_Renderer* renderer, program_state state);
 
 // Offset functions make it easy to have multiple viewports
 void draw_line_with_offset(SDL_Renderer* renderer, line l, SDL_Point offset);
--- src/render_utils.c
+++ src/render_utils.c
@@ -1,5 +1,51 @@
 #include "render_utils.h"
 
+#include "parameters.h"
+
+#include <string.h>
+
+void draw_debug_text(SDL_Renderer* renderer, program_state state)
+{
+    static char message[1024];
+}
+
+void draw_views(SDL_Renderer* renderer, SDL_Point* offset)
+{
+    // draw absolute viewport
+    // set color to black & draw viewport background
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &ABSOLUTE_VIEW);
+    // set color to red and draw border.
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &ABSOLUTE_VIEW);
+
+    // draw transformed viewport
+    // set color to black & draw viewport background
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &TRANSFORMED_VIEW);
+    // set color to red and draw border.
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &TRANSFORMED_VIEW);
+
+    offset->x = TRANSFORMED_VIEW.x;
+    offset->y = TRANSFORMED_VIEW.y;
+
+    // draw player line
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    line l = { { HALF_VIEW_WIDTH, HALF_VIEW_HEIGHT }, { HALF_VIEW_WIDTH, HALF_VIEW_WIDTH - 5 } };
+    draw_line_with_offset(renderer, l, *offset);
+
+    // change render colour to a light blue for viewplane trim
+    SDL_SetRenderDrawColor(renderer, 128, 128, 255, 128);
+
+    // Draw intersecting line
+    const float eps = 1e-4;
+    line l1 = { { HALF_VIEW_WIDTH - eps, HALF_VIEW_WIDTH - eps }, { HALF_VIEW_WIDTH - (HALF_VIEW_WIDTH / 2), 0 } };
+    line l2 = { { HALF_VIEW_WIDTH + eps, HALF_VIEW_WIDTH - eps }, { HALF_VIEW_WIDTH + (HALF_VIEW_WIDTH / 2), 0 } };
+    draw_line_with_offset(renderer, l1, *offset);
+    draw_line_with_offset(renderer, l2, *offset);
+}
+
 void draw_line_with_offset(SDL_Renderer* renderer, line l, SDL_Point offset)
 {
     SDL_RenderDrawLine(renderer, 
